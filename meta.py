eb277969e99606b7cb06293adfe62a65b0230ea1
Leonid Kostrykin <void@evoid.de>
2025-03-27 19:17:30 +0000

    Automatically determine prefix, remove parameter

--- meta.py
+++ meta.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 
-import argparse
 import pathlib
 import os
 from datetime import date
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--prefix', type=str, default='')
-args = parser.parse_args()
+def expand(today):
+    year = today.year % 2000
+    if 4 <= today.month <= 8:
+        prefix = 'ss'
+    else:
+        prefix = 'ws'
+
+    branch_name = f'{prefix}{year + 1}'
+    branch = f'future/{branch_name}'
+
+    return {
+        'branch-name': branch_name,
+        'branch': branch,
+    }
 
 
-today = date.today()
-year = today.year % 2000
-
-branch_name = f'{args.prefix}{year + 1}'
-branch = f'future/{branch_name}'
-
-print(f'branch-name={branch_name}')
-print(f'branch={branch}')
+if __name__ == '__main__':
+    today = date.today()
+    data = expand(today)
+    for key, value in data.items():
+        print(f'{key}={value}')
