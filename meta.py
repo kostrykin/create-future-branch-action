92160c7a414236fe82990327dfb2c4127c73ab81
Leonid Kostrykin <void@evoid.de>
2025-03-27 16:35:10 +0000

    Migrate to `peter-evans/create-pull-request` action

---
+++ meta.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import argparse
+import pathlib
+import os
+from datetime import date
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--prefix', type='str', default='')
+args = parser.parse_args()
+
+
+today = date.today()
+year = today.year % 2000
+
+branch_name = f'{args.prefix}{year + 1}'
+branch = f'future/{branch_name}'
+
+print(f'branch-name={branch_name}')
+print(f'branch={branch}')
